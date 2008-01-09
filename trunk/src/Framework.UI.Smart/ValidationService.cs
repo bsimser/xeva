4bb9b46b91894d19770c05bfc85886a24f865004
dinatale.anthony <dinatale.anthony@fee9bd96-5c42-0410-9105-39764ee1010e>
2008-01-09 00:28:10 +0000

    Added a Validate method to Presenter; Added a service to front the Validator in Shared

---
+++ trunk/src/Framework.UI.Smart/ValidationService.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using XEVA.Framework.Validation;
+
+namespace XEVA.Framework.UI.Smart
+{
+   public class ValidationService : IValidationService
+   {
+      public virtual bool Validate(object target, Dictionary<string, IControl> controls)
+      {
+         Dictionary<string, IValidationAware> validationObjects = new Dictionary<string, IValidationAware>();
+
+         foreach (KeyValuePair<string, IControl> control in controls)
+         {
+            validationObjects.Add(control.Key, control.Value);
+         }
+
+         return new Validator().Validate(target, validationObjects).IsValid;
+      }
+   }
+}
