00ea78034925ca768f664a93a05a845ecb3ac606
dinatale.anthony <dinatale.anthony@fee9bd96-5c42-0410-9105-39764ee1010e>
2008-01-09 21:25:11 +0000

    Added a null check on the result in the request; Added test specs for the request

---
+++ trunk/src/Framework.UI.Smart.Specs/RequestSpecs.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using XEVA.Framework.Specs;
+using XEVA.Framework.UI.Smart;
+
+namespace Specs_for_Request
+{
+   [TestFixture]
+   public class When_adding_an_item_to_the_request : Spec
+   {
+      private Request _request;
+
+      protected override void Before_each_spec()
+      {
+         _request = new Request();
+      }
+
+      [Test]
+      public void The_item_should_be_retrievable_from_the_request()
+      {
+         object item1 = new object();
+
+         _request.SetItem("item1", item1);
+
+         Assert.AreEqual(item1, _request.GetItem("item1", new object()));
+      }
+   }
+
+   [TestFixture]
+   public class When_retrieving_an_item_from_the_request : Spec
+   {
+      private Request _request;
+
+      protected override void Before_each_spec()
+      {
+         _request = new Request();
+      }
+
+      [Test]
+      public void Return_the_empty_value_if_the_item_is_not_in_the_request()
+      {
+         object item1 = new object();
+
+         Assert.AreEqual(item1, _request.GetItem("item1", item1));
+      }
+
+      [Test, ExpectedException(typeof(MissingRequiredRequestItemException))]
+      public void Fail_if_a_required_item_is_not_in_the_request()
+      {
+         _request.GetItem("item1", new object(), true);
+      }
+   }
+}
