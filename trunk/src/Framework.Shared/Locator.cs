122f4c3e9df6ac8528468446688cabc1473335cc
laribee <laribee@fee9bd96-5c42-0410-9105-39764ee1010e>
2008-05-09 20:26:14 +0000

    Misc changes.

--- trunk/src/Framework.Shared/Locator.cs
+++ trunk/src/Framework.Shared/Locator.cs
@@ -17,19 +17,43 @@ namespace XF
 
       public static T Resolve<T>()
       {
+         GuardContainerInitialized();
          return Container.Resolve<T>();
       }
 
       public static T Resolve<T>(string name)
       {
+         GuardContainerInitialized();
          return Container.Resolve<T>(name);
       }
 
-      // This method is used to return objects in the RemoteProxy.
-      // Types are unknown at the time the objects are created.
       public static object Resolve(string component)
       {
+         GuardContainerInitialized();
          return Container.Resolve(component);
+      }
+
+      public static void AddComponent(string componentKey, Type componentType)
+      {
+         try
+         {
+            Container.AddComponent(componentKey, componentType);
+         }
+         catch (ComponentRegistrationException)
+         {
+         }
+      }
+
+      public static void Reset()
+      {
+         if (Container == null) return;
+         Container.Dispose();
+         Container = null;
+      }
+
+      public static bool Initialized
+      {
+         get { return Container != null; }
       }
 
       private static IWindsorContainer Container
@@ -45,28 +69,9 @@ namespace XF
          }
       }
 
-      public static bool Initialized
+      private static void GuardContainerInitialized()
       {
-         get { return Container != null; }
-      }
-
-      public static void Reset()
-      {
-         if (Container == null) return;
-         Container.Dispose();
-         Container = null;
-
-      }
-
-      public static void AddComponent(string componentKey, Type componentType)
-      {
-         try
-         {
-            Container.AddComponent(componentKey, componentType);
-         }
-         catch (ComponentRegistrationException)
-         {
-         }
+         if (!Initialized) throw new InvalidOperationException("Container is not initialized.");
       }
    }
 }
