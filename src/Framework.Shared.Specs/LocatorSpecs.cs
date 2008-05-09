6628cb4c456ebfc7a53d90f67e1fa391fa4d8cb8
laribee <laribee@fee9bd96-5c42-0410-9105-39764ee1010e>
2008-05-09 20:26:14 +0000

    Misc changes.

--- src/Framework.Shared.Specs/LocatorSpecs.cs
+++ src/Framework.Shared.Specs/LocatorSpecs.cs
@@ -1,3 +1,4 @@
+using System;
 using Castle.Windsor;
 using NUnit.Framework;
 using XF;
@@ -6,11 +7,12 @@ using XF.Specs;
 namespace Specs_for_Locator
 {
    [TestFixture]
-   public class Before_using_a_container : Spec
+   public class Before_resolving_a_component : Spec
    {
       [Test]
       public void You_must_initialize_with_a_backing_windsor_container()
       {
+         Locator.Reset();
          Assert.IsFalse(Locator.Initialized);
          Locator.Initialize(new WindsorContainer());
          Assert.IsTrue(Locator.Initialized);
@@ -19,7 +21,7 @@ namespace Specs_for_Locator
    }
 
    [TestFixture]
-   public class When_using_a_container : Spec
+   public class When_resolving_a_component : Spec
    {
       [Test]
       public void You_should_be_able_to_resolve_a_generic_type()
@@ -38,6 +40,27 @@ namespace Specs_for_Locator
          Assert.AreEqual(Locator.Resolve<Spec>("test").GetType(), typeof (Spec));
          Locator.Reset();
       }
+
+      [Test]
+      public void You_should_be_able_to_resolve_a_component_by_key_only()
+      {
+         Locator.Initialize(new WindsorContainer());
+         Locator.AddComponent("test", typeof(Spec));
+         Assert.AreEqual(Locator.Resolve("test").GetType(), typeof(Spec));
+         Locator.Reset();
+      }
+
+      [Test, ExpectedException(typeof(InvalidOperationException))]
+      public void Throw_an_exception_if_the_container_is_not_initialized_when_getting_a_component_through_generics()
+      {
+         Locator.Resolve<Spec>();
+      }
+
+      [Test, ExpectedException(typeof(InvalidOperationException))]
+      public void Throw_an_exception_if_the_container_is_not_initialized_when_getting_a_component_as_an_objects()
+      {
+         Locator.Resolve(string.Empty);
+      }
    }
 
 
@@ -47,6 +70,7 @@ namespace Specs_for_Locator
       [Test]
       public void The_container_should_be_emptied_out()
       {
+         Locator.Reset();
          Assert.IsFalse(Locator.Initialized);
          Locator.Initialize(new WindsorContainer());
          Locator.Reset();
@@ -67,4 +91,5 @@ namespace Specs_for_Locator
          Locator.Reset();
       }
    }
+
 }
