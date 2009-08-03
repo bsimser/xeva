c7c3ae9fbf610b47b5501914ad4f1688ad5e43f8
rik.bardrof@gmail.com <rik.bardrof@gmail.com@fee9bd96-5c42-0410-9105-39764ee1010e>
2009-08-03 14:19:15 +0000

    Added type safety to RegisterControl in Presenter.

--- trunk/src/Framework.UI.Smart/Presenter/IViewCallbacks.cs
+++ trunk/src/Framework.UI.Smart/Presenter/IViewCallbacks.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Linq.Expressions;
+
 namespace XF.UI.Smart
 {
    public interface IViewCallbacks
    {
       void RegisterControl(string property, IControl control);
+      void RegisterControl<TMessage>(Expression<Func<TMessage, Object>> expression, IControl control);
    }
 }
