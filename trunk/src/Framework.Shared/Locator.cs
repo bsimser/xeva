5bdf66bc912ec37c093d9498d41663ffd10598c7
rik.bardrof@gmail.com <rik.bardrof@gmail.com@fee9bd96-5c42-0410-9105-39764ee1010e>
2011-05-24 15:24:49 +0000

    Added component lookup verification to the windsor container, Querably to the store.

--- trunk/src/Framework.Shared/Locator.cs
+++ trunk/src/Framework.Shared/Locator.cs
@@ -1,81 +1,75 @@
 using System;
 using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 // ayende credit
 
-namespace XF
-{
-   public static class Locator
-   {
+namespace XF {
+   public static class Locator {
 
       private const string CONTAINER_KEY = "XF.Shared:CONTAINER_KEY";
 
-      public static void Initialize(IWindsorContainer windsorContainer)
-      {
+      public static void Initialize(IWindsorContainer windsorContainer) {
          Container = windsorContainer;
       }
 
-      public static T Resolve<T>()
-      {
+      public static bool HasComponent(string key) {
+         GuardContainerInitialized();
+         return Container.Kernel.HasComponent(key);
+      }
+
+      public static T Resolve<T>() {
          GuardContainerInitialized();
          return Container.Resolve<T>();
       }
 
-      public static T Resolve<T>(string name)
-      {
+      public static T Resolve<T>(string name) {
          GuardContainerInitialized();
          return Container.Resolve<T>(name);
       }
 
-      public static object Resolve(string component)
-      {
+      public static object Resolve(string component) {
          GuardContainerInitialized();
          return Container.Resolve(component);
       }
 
-      public static void Release(object toRelease)
-      {
+      public static void Release(object toRelease) {
          Container.Release(toRelease);
       }
 
-      public static void AddComponent(string componentKey, Type componentType)
-      {
-         try
-         {
+      public static void Register(IRegistration[] registrations) {
+         Container.Register(registrations);
+      }
+
+      public static void AddComponent(string componentKey, Type componentType) {
+         try {
             Container.AddComponent(componentKey, componentType);
          }
-         catch (ComponentRegistrationException)
-         {
+         catch (ComponentRegistrationException) {
          }
       }
 
-      public static void Reset()
-      {
+      public static void Reset() {
          if (Container == null) return;
          Container.Dispose();
          Container = null;
       }
 
-      public static bool Initialized
-      {
+      public static bool Initialized {
          get { return Container != null; }
       }
 
-      private static IWindsorContainer Container
-      {
-         get
-         {
+      private static IWindsorContainer Container {
+         get {
             var result = Globals.Data[CONTAINER_KEY] as IWindsorContainer;
             return result;
          }
-         set
-         {
+         set {
             Globals.Data[CONTAINER_KEY] = value;
          }
       }
 
-      private static void GuardContainerInitialized()
-      {
+      private static void GuardContainerInitialized() {
          if (!Initialized) throw new InvalidOperationException("Container is not initialized.");
       }
    }
