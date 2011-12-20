b2ac183d3b51efb72e57f1ccb233a5c4f5a37df2
rik.bardrof@gmail.com <rik.bardrof@gmail.com@fee9bd96-5c42-0410-9105-39764ee1010e>
2011-12-20 15:09:31 +0000

    Null check on RepositoryHelper ToShortDate()

--- trunk/src/Framework.Model/RepositoryHelper.cs
+++ trunk/src/Framework.Model/RepositoryHelper.cs
@@ -3,7 +3,7 @@ using System;
 namespace XF.Model {
    public static class RepositoryHelper {
       public static string ToShortDate(object date) {
-         return ((DateTime)date).ToShortDateString();
+         return date != null && date is DateTime ? ((DateTime)date).ToShortDateString() : string.Empty;
       }
    }
 }
